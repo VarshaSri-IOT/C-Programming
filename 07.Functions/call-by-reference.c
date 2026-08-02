6bd41b166ca1db1b1a0e6344ca6853549c5f8e7b
VarshaSri-IOT <varshasrikankata.05@gmail.com>
2026-08-02 06:27:06 +0530

    Add Functions module and with documentation

---
+++ 07.Functions/call-by-reference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void change(int *x)
+{
+    *x = 100;
+    printf("Inside Function: *x = %d\n", *x);
+}
+
+int main()
+{
+    int a = 10;
+
+    printf("Before Function: a = %d\n", a);
+
+    change(&a);
+
+    printf("After Function: a = %d\n", a);
+
+    return 0;
+}
---
+++ 07.Functions/swap-using-call-by-reference.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+
+    printf("Before Swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After Swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
